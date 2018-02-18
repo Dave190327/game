93e4e1029ca80ff7189ed6595bb8bf241a8d6f79
Soul327 <soulsilver27@hotmail.com>
2018-02-18 10:21:49 -0600

    test

--- Game/src/Launcher.java
+++ Game/src/Launcher.java
@@ -2,7 +2,7 @@
 public class Launcher {
 	public static void main(String[] args){
 		Game game = new Game("Space Cards", 1600, 900);
-		System.out.println("Test");
+		System.out.println("works?");
 		game.start();
 	}
 }
